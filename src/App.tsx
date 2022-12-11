f1a082772a715b087d1fa768eec2ae8f2947d013
benzson92 <benzson.v@gmail.com>
2022-12-11 18:27:08 +0700

    react root files

---
+++ src/App.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import {
+	BrowserRouter as Router,
+	Routes,
+	Route,
+} from 'react-router-dom';
+
+import { ThemeProvider } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+
+import theme from './assets/theme';
+
+import Home from './routes/home';
+import Coin from './routes/coin';
+
+function App() {
+	return (
+		<ThemeProvider theme={theme}>
+			<Router>
+				<Box padding="24px" paddingX="8px">
+					<Routes>
+						<Route
+							path="/"
+							element={<Home />}
+						/>
+						<Route
+							path="/coins/:id"
+							element={<Coin />}
+						/>
+					</Routes>
+				</Box>
+			</Router>
+		</ThemeProvider>
+	);
+}
+
+export default App;
